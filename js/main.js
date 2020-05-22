fb9266129bc73fb34d14b2a2e83f897bd0b929ff
Jose-cod7 <josegarriaga7@gmail.com>
2020-05-22 16:23:47 +0200

    crud-remove-part

--- js/main.js
+++ js/main.js
@@ -16,19 +16,29 @@ function upDateTable(data) {
     data.forEach((obj) => {
         console.log(obj);
         const tr = document.createElement("tr");
+
         const thTitle = document.createElement("th");
         const thHref = document.createElement("th");
         const thEdit = document.createElement("th");
         const thRemove = document.createElement("th");
 
-        const link = document.createElement("a");
-        link.setAttribute("href", obj.href);
-        link.innerText = obj.href;
+        const hreflink = document.createElement("a");
+        hreflink.setAttribute("href", obj.href);
+        hreflink.innerText = obj.href;
 
         thTitle.innerText = obj.title;
+
+        const editLink = document.createElement("a");
+        editLink.setAttribute("href", "edit.html?id=232454");
+        editLink.innerText = "Edit";
+
+        const removeLink = document.createElement("a");
+        removeLink.innerText = "Remove";
+
         tr.appendChild(thTitle);
-        thHref.innerText = obj.href;
-        tr.appendChild(link);
+        tr.appendChild(thHref).appendChild(hreflink);
+        tr.appendChild(thEdit).appendChild(editLink);
+        tr.appendChild(thRemove).appendChild(removeLink);
 
         tbody.appendChild(tr);
     });
